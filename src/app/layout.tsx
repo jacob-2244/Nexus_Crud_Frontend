c3bea431224d7a1c7936264a973198f778fb18c5
Muhammad Yaqoob <muhammadyaqoob2580@gmail.com>
2025-12-09 15:23:54 +0500

    removed hydration error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import React from "react";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning> 
-      <body className="flex flex-col min-h-screen bg-[var(--app-background)]">
+      <body className="flex flex-col h-screen bg-[var(--app-background)]">
         <ReduxProvider>
           <LayoutClient>{children}</LayoutClient>
         </ReduxProvider>
